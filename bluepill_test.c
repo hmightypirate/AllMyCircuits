549c5b78b30c73977752c7fc7f429f04b8a617b3
hmightypirate <hcerezoc@gmail.com>
2018-02-17 17:36:02 +0100

    Added basic coafile

--- bluepill_test.c
+++ bluepill_test.c
@@ -3,7 +3,8 @@
 
 int main(void) {
     rcc_periph_clock_enable(RCC_GPIOC);
-    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL,
+                  GPIO13);
 
     while(1) {
         gpio_set(GPIOC, GPIO13);
