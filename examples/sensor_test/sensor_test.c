2818ce06a7546e44340be7e50212407f50b6ece0
Jorge Muñoz Castañer <punkto@gmail.com>
2018-02-20 10:42:14 +0100

    Fix printf in sensor_test.c

--- examples/sensor_test/sensor_test.c
+++ examples/sensor_test/sensor_test.c
@@ -22,7 +22,23 @@
 #include <libopencm3/stm32/usart.h>
 #include <libopencm3/stm32/adc.h>
 #include <stdio.h>
+#include <errno.h>
 #include <unistd.h>
+
+
+int _write(int file, char *ptr, int len)
+{
+	int i;
+
+	if (file == 1) {
+		for (i = 0; i < len; i++)
+			usart_send_blocking(USART1, ptr[i]);
+		return i;
+	}
+
+	errno = EIO;
+	return -1;
+}
 
 static void clock_setup(void)
 {
