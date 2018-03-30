428171ca02c1c6da70b99b50912a03b01ab796a7
David Chaves <dchavesd@gmail.com>
2018-03-30 18:09:00 +0200

    Example to move a micro servo 9g A0090

--- rules.mk
+++ rules.mk
@@ -2,23 +2,27 @@ CROSS_COMPILE = arm-none-eabi-
 CC = $(CROSS_COMPILE)gcc
 OBJCOPY = $(CROSS_COMPILE)objcopy
 CPUFLAGS = -mcpu=cortex-m3 -mthumb
-LIBPATHS := ./libopencm3 ../../libopencm3 
+LIBPATHS := ./libopencm3 ../../libopencm3
+
+### CHANGE THIS LINE TO SELECT BLUEPILL OR BLACKPILL
+#PILL = BLACK
+
+ifeq ($(PILL), BLACK)
+PILLFLAGS = -DINTERNAL_LED=GPIO12 -DINTERNAL_LED_PORT=GPIOB -DRCC_INTERNAL_LED=RCC_GPIOB
+else
+PILLFLAGS = -DINTERNAL_LED=GPIO13 -DINTERNAL_LED_PORT=GPIOC -DRCC_INTERNAL_LED=RCC_GPIOC
+endif
 
 OPENCM3_DIR := $(wildcard $(LIBPATHS:=/locm3.sublime-project))
 OPENCM3_DIR := $(firstword $(dir $(OPENCM3_DIR)))
-
-
 
 ifeq ($(strip $(OPENCM3_DIR)),)
 $(warning Cannot find libopencm3 library in the standard search paths.)
 $(error Please specify it through OPENCM3_DIR variable!)
 endif
 
-
-CFLAGS = -Wall -Wextra -g3 -O0 -MD $(CPUFLAGS) -DSTM32F1 -I${OPENCM3_DIR}/include -std=c99
-
+CFLAGS = -Wall -Wextra -g3 -O0 -MD $(CPUFLAGS) -DSTM32F1 -I${OPENCM3_DIR}/include -std=c99 $(PILLFLAGS)
 LDLIBS = -lopencm3_stm32f1 -lc -lnosys
-
 
 LIBPATHS := . ../../
 BLUE_DIR := $(wildcard $(LIBPATHS:=/bluepill.ld))
@@ -27,7 +31,6 @@ ifeq ($(strip $(BLUE_DIR)),)
 $(warning Cannot find bluepill.ld in the standard search paths.)
 $(error Please specify it through BLUE_DIR variable!)
 endif
-
 
 LDFLAGS = $(CPUFLAGS) -nostartfiles -L${OPENCM3_DIR}/lib -Wl,-T,${BLUE_DIR}/bluepill.ld
 
@@ -38,5 +41,3 @@ libopencm3:
 		git submodule update; \
 	fi
 	$(MAKE) -C ${OPENCM3_DIR} lib/stm32/f1
-
-
