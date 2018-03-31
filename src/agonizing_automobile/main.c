2451a51d8abbf8834687fc0fc50b4c11c8ac97dc
hmightypirate <hcerezoc@gmail.com>
2018-03-31 16:47:53 +0200

    Added sensor functions

--- src/agonizing_automobile/main.c
+++ src/agonizing_automobile/main.c
@@ -1,6 +1,7 @@
 #include "setup.h"
 #include "pid.h"
 #include "motors.h"
+#include "sensors.h"
 
 /**
  * @brief Initial setup and main loop
@@ -15,15 +16,19 @@ int main(void)
   reset_target_velocity(INITIAL_TARGET_VELOCITY);
 
   /* reset sensors */
-  //TODO
+  //FIXME: better do some callibration
+  hard_reset_sensors();
   
   /* reset pid */
   reset_pid();
 
+  //FIXME: do some state machine here (callibration, running, etc)
   while(1)
     {
-      //TODO read sensors
-      int proportional = 0;
+      /* read data from sensors */
+      uint16_t sensor_value[NUM_SENSORS];
+      read_line_sensors(sensor_value);
+      int proportional = get_line_position(sensor_value);
       
       /* pid control */
       int error = 0;
