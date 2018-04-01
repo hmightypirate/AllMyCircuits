cd8e0e8536d16560434c84f6c2d7458e4d8ce4fd
eLeDe <little_elede@hotmail.com>
2018-04-01 16:42:23 +0200

    Basic subroutine example

--- examples/cli_test/cli_test.c
+++ examples/cli_test/cli_test.c
@@ -1,33 +1,40 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
+#include <libopencm3/cm3/nvic.h>
 
 static void clock_setup(void);
 static void usart_setup(void);
 static void gpio_setup(void);
 
-char welcome[] = "Echo test\n";
-#define WELCOME_SIZE 10
+void send_message(char *message);
+void send_usart(char *message);
 
-
-
+void usart1_isr(void) {
+    if (((USART_CR1(USART1) & USART_CR1_RXNEIE) != 0) &&
+    ((USART_SR(USART1) & USART_SR_RXNE) != 0)) {
+        send_message("usart1_isr\n");
+    }
+}
 
 int main(void) {
     clock_setup();
     gpio_setup();
     usart_setup();
 
-    for (int i = 0; i < WELCOME_SIZE; i++){
-        usart_send_blocking(USART1, welcome[i]);
-    }
+    send_message("Agonizing Automobile CLI by Mighty Escuderida\n");
+    send_message("Version v0.1\n");
+
     while (1) {
         gpio_toggle(GPIOC, GPIO13); /* LED on/off */
-        usart_send_blocking(USART1, usart_recv_blocking(USART1));
+        for (int i = 0; i < 10000000; ++i) __asm__("nop");
+        //while (usart_get_flag(USART1, USART_SR_RXNE)) {
+        //    usart_send_blocking(USART1, usart_recv_blocking(USART1));
+        //}
+        
+        //usart_send_blocking(USART1, usart_recv(USART1));
     }
 }
-
-
-
 
 static void clock_setup(void) {
     rcc_clock_setup_in_hse_8mhz_out_72mhz();
@@ -42,6 +49,9 @@ static void clock_setup(void) {
 }
 
 static void usart_setup(void) {
+    nvic_set_priority(NVIC_USART1_IRQ, 16);
+    nvic_enable_irq(NVIC_USART1_IRQ);
+    
     gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_INPUT_PULL_UPDOWN,
             GPIO_USART1_TX);
     gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
@@ -55,6 +65,7 @@ static void usart_setup(void) {
     usart_set_parity(USART1, USART_PARITY_EVEN);
     usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
 
+    usart_enable_rx_interrupt(USART1);
     /* Finally enable the USART. */
     usart_enable(USART1);
 }
@@ -63,4 +74,15 @@ static void gpio_setup(void) {
     /* Set GPIO12 (in GPIO port C) to 'output push-pull'. */
     gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL,
             GPIO13);
+}
+
+
+void send_message(char *message) {
+    send_usart(message);
+}
+
+void send_usart(char *message) {
+    for (int i = 0; i < strlen(message); i++){
+        usart_send_blocking(USART1, message[i]);
+    }
 }
