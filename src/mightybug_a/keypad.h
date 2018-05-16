abb534b5216975d548938760f2175f332ef856b5
hmightypirate <hcerezoc@gmail.com>
2018-05-16 20:27:10 +0200

    Adding functions to detecting edges

--- src/mightybug_a/keypad.h
+++ src/mightybug_a/keypad.h
@@ -6,10 +6,15 @@
 /* Debounce period defined in ms */
 #define DEBOUNCE_PERIOD 10
 
+void keypad_loop(uint32_t current_millis);
 void keypad_setup(uint32_t current_millis,
                   uint16_t *port_array,
                   uint16_t *pin_array);
 
+
 bool get_button(uint8_t id_button);
+void unset_edge(uint8_t id_button);
+bool get_edge_detection(uint8_t id_button);
+
 
 #endif /* __KEYPAD_H */
