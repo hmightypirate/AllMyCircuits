04ae7a668360fa84c0c0f19bbe40ac28f3e34f41
eLeDe <little_elede@hotmail.com>
2018-05-19 17:45:56 +0200

    Added pid partial values to telemetry

--- src/mightybug_a/telemetry.c
+++ src/mightybug_a/telemetry.c
@@ -7,9 +7,12 @@ void print_telemetry()
     if (SHOW_K_P) printf("%d,", get_kp());
     if (SHOW_K_I) printf("%d,", get_ki());
     if (SHOW_K_D) printf("%d,", get_kd());
+    if (SHOW_P_VALUE) printf("%d,", get_proportional());
+    if (SHOW_I_VALUE) printf("%d,", get_integral());
+    if (SHOW_D_VALUE) printf("%d,", get_derivative());
     if (SHOW_LEFT_PWM) printf("%d,", get_last_left_vel());
     if (SHOW_RIGHT_PWM) printf("%d,", get_last_right_vel());
-    if (SHOW_LEFT_ENCODER) printf("%d,", get_left_velocity());
-    if (SHOW_RIGHT_ENCODER) printf("%d,", get_right_velocity());
-    printf("\b\n");
+    if (SHOW_LEFT_ENCODER) printf("%u,", (unsigned int)get_left_velocity());
+    if (SHOW_RIGHT_ENCODER) printf("%u,", (unsigned int)get_right_velocity());
+    printf("0\n");
 }
--- src/mightybug_a/telemetry.h
+++ src/mightybug_a/telemetry.h
@@ -4,7 +4,12 @@
 #define SHOW_K_P 1
 #define SHOW_K_I 1
 #define SHOW_K_D 1
+#define SHOW_P_VALUE 1
+#define SHOW_I_VALUE 1
+#define SHOW_D_VALUE 1
 #define SHOW_LEFT_PWM 1
 #define SHOW_RIGHT_PWM 1
 #define SHOW_LEFT_ENCODER 1
 #define SHOW_RIGHT_ENCODER 1
+
+void print_telemetry();
