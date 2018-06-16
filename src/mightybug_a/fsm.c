51d1eda9f8c101b8febaf49337537f9eeecf1880
hmightypirate <hcerezoc@gmail.com>
2018-06-16 17:50:05 +0200

    bug, wrong name of function

--- src/mightybug_a/fsm.c
+++ src/mightybug_a/fsm.c
@@ -50,7 +50,7 @@ void update_state(event_e new_event)
         }
       else if (new_event == GO_TO_RUN_EVENT)
         {
-          if (get_calibrated_sensors() >= NUM_SENSORS -
+          if (get_calibrated_sensors_count() >= NUM_SENSORS -
               MAX_NUM_NOT_CALLIBRATED_SENSORS)
             {
               current_state = RUNNING_STATE;
