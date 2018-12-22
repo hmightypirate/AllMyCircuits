6e733452594cfc2fd913d2406a0ab29f08ddbc1c
eLeDe <little_elede@hotmail.com>
2018-12-22 21:00:48 +0100

    Refactor keypadmenu module

--- src/mightybug_a/keypad_menu.c
+++ src/mightybug_a/keypad_menu.c
@@ -1,42 +1,46 @@
 #include "keypad_menu.h"
 
+bool button_released(uint8_t id_button)
+{
+  return (get_edge_detection(id_button) && !get_button(id_button));
+}
+
+bool button_pressed(uint8_t id_button)
+{
+  return (get_edge_detection(id_button) && get_button(id_button));
+}
 
 void menu_functions(void)
 {
-  uint32_t current_millis = get_millisecs_since_start();
-  
+  uint32_t millis = get_millisecs_since_start();
+
   // Button delayed start (falling edge)
-  if (!get_button(BUTTON_START) &&
-      get_edge_detection(BUTTON_START))
-    {
-      set_delay_start_time(current_millis);
-      update_state(GO_TO_DELAYED_START_EVENT);
-    }
+  if (button_released(BUTTON_START))
+  {
+    set_delay_start_time(millis);
+    update_state(GO_TO_DELAYED_START_EVENT);
+  }
 
   // Button delayed start: Resetting state (rising edge)
   /*
-  if (get_button(BUTTON_START) &&
-      get_edge_detection(BUTTON_START))
+  if (button_pressed(BUTTON_START))
     {
       update_state(FORCE_CALLIBRATION_EVENT);
     }
   */
 
   // Pid and vel mapping (rising edge)
-  if (get_button(BUTTON_PIDANDVEL_MAPPING) &&
-      get_edge_detection(BUTTON_PIDANDVEL_MAPPING))
-    {
-      set_pidvel_map_time(current_millis);
-      update_state(NEXT_PIDANDVELMAP_EVENT);
-    }
+  if (button_pressed(BUTTON_PIDANDVEL_MAPPING))
+  {
+    set_pidvel_map_time(millis);
+    update_state(NEXT_PIDANDVELMAP_EVENT);
+  }
 
   // Buzzer on/off mapping (rising edge)
-  if (get_button(BUTTON_BUZZER) &&
-      get_edge_detection(BUTTON_BUZZER))
-    {
-      
-      update_state(NEXT_BUZZER_EVENT);
-    }
+  if (button_pressed(BUTTON_BUZZER))
+  {
+    update_state(NEXT_BUZZER_EVENT);
+  }
 
   // setting all the edges to false
   clear_edge_detected(BUTTON_START);
