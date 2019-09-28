86239e1218c42e85c7c4d130274c6ceebd177c62
eLeDe <little_elede@hotmail.com>
2019-09-28 20:12:45 +0200

    Fix cli MAP ALL print values

--- src/mightybug_a/cli.c
+++ src/mightybug_a/cli.c
@@ -215,7 +215,7 @@ static void cli_map_all() {
     idx_map = 0;
   //for (int i = 0; i < MAX_MAP_STATES; i++)
   //  {
-      printf("%i State: %d, Ticks left: %zu, Ticks right: %zu Total: %zu\n",
+      printf("%i State: %d, Ticks left: %u, Ticks right: %u Total: %u\n",
 	     idx_map,
 	     last_map.mapstates[idx_map],
 	     (unsigned int) last_map.agg_left_ticks[idx_map],
