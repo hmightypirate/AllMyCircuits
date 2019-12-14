135c579539705db4c8e7da20a5626a14a5a2e961
eLeDe <little_elede@hotmail.com>
2019-12-14 19:52:30 +0100

    Feature shutdown line sensor (#182)
    * Add delay function
    
    * Enable and disable line sensors on the fly
    
    * Enable cycle counter
    
    * Ensure disable sensors at idle

--- src/mightybug_a/cron.c
+++ src/mightybug_a/cron.c
@@ -24,3 +24,16 @@ uint32_t get_millisecs_since_start()
 {
 	return millisecs_since_start;
 }
+
+
+void delay_microseconds(uint32_t microseconds)
+{
+	// FIXME: Bug if now + microseconds * 72 is close to UINT32_MAX
+	uint32_t now = dwt_read_cycle_counter();
+	if (now + microseconds * 72 < microseconds * 72) {
+		while (dwt_read_cycle_counter() > now);
+	}
+	
+	while (dwt_read_cycle_counter() < now + microseconds * 72);
+
+}
--- src/mightybug_a/cron.h
+++ src/mightybug_a/cron.h
@@ -3,7 +3,9 @@
 
 #include "encoder.h"
 #include <libopencm3/cm3/systick.h>
+#include <libopencm3/cm3/dwt.h>
 
 uint32_t get_millisecs_since_start();
+void delay_microseconds(uint32_t microseconds);
 
 #endif /* __CRON_H */
